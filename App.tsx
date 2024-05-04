98f39b4e28fde3292c7235fd848b3e808885425a
RayKay91 <61206305+RayKay91@users.noreply.github.com>
2024-05-04 15:47:09 +0100

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,73 +1,39 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
-import {ScrollView, StyleSheet, Text, useColorScheme, View} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+import React, {useCallback, useEffect, useRef} from 'react';
+import {Animated, StyleSheet, Text, useColorScheme, View} from 'react-native';
 
 function App(): React.JSX.Element {
+  const r = useRef(new Animated.Value(0)).current;
+  const animate = useCallback(() => {
+    Animated.timing(r, {
+      toValue: 1,
+      useNativeDriver: true,
+      duration: 2000,
+    }).start();
+  }, [r]);
+
+  useEffect(() => {
+    setTimeout(animate, 2000);
+  }, [animate]);
+  const inputRange = [0, 1];
+  const outputRange = ['0deg', '360deg'];
   return (
-    <ScrollView contentInsetAdjustmentBehavior="automatic">
-      <Header />
-      <View>
-        <Section title="Step One">
-          Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-          screen and then come back to see your edits.
-        </Section>
-        <Section title="See Your Changes">
-          <ReloadInstructions />
-        </Section>
-        <Section title="Debug">
-          <DebugInstructions />
-        </Section>
-        <Section title="Learn More">
-          Read the docs to discover what to do next:
-        </Section>
-        <LearnMoreLinks />
-      </View>
-    </ScrollView>
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderWidth: 10,
+        borderColor: 'lime',
+      }}>
+      <Animated.View
+        style={{
+          transform: [{rotate: r.interpolate({inputRange, outputRange})}],
+          backgroundColor: 'red',
+          height: 100,
+          width: 100,
+        }}
+      />
+    </View>
   );
 }
 
